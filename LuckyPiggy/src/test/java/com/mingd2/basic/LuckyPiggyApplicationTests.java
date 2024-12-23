6398d8abdda2df09c2b42fd063d3df8319257bec
mingd2 <rkalej@naver.com>
2024-12-23 15:01:36 +0900

    test 제발

---
+++ LuckyPiggy/src/test/java/com/mingd2/basic/LuckyPiggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingd2.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyPiggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
