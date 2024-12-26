24f69fdf34ecd4503232cb7ec1305809050e0182
USER <USER@192.168.219.249>
2024-12-26 12:30:38 +0900

    DB연결

--- LuckyPiggy/src/main/java/com/tom/basic/controller/MainController.java
+++ LuckyPiggy/src/main/java/com/tom/basic/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.tom.basic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tom.basic.entity.TbUser;
+import com.tom.basic.model.UserVO;
+import com.tom.basic.repository.UserRepo;
 
 @Controller
 public class MainController {
@@ -41,5 +47,26 @@ public class MainController {
 	public String calendar() {
 		return "calendar";
 	}
+	
+	
+	
+	
+	@Autowired
+	UserRepo repo;
+	
+	
+	// 회원가입 기능
+	@PostMapping("/join.do")
+	public String join(UserVO vo) {
+		
+		TbUser en = new TbUser(vo);
+		
+		repo.save(en);
+		
+		return "redirect:/";
+	}
+	
+	
+	
 	
 }
