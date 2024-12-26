de26c0e235e83140177d42c2769acf9012b624eb
smhrd <smhrd@192.168.219.49>
2024-12-26 16:37:27 +0900

    DDAHUHUK

--- LuckyPiggy/src/main/java/com/tom/basic/controller/MainController.java
+++ LuckyPiggy/src/main/java/com/tom/basic/controller/MainController.java
@@ -1,60 +1,88 @@
 package com.tom.basic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tom.basic.entity.GraphEntity;
+import com.tom.basic.repository.GraphRepo;
 
 @Controller
 public class MainController {
 
-	
 	@GetMapping("/")
 	public String home() {
 		return "index";
 	}
-	
+
 	@GetMapping("/daily")
 	public String daily() {
 		return "daily";
 	}
-	
+
 	@GetMapping("/join")
 	public String join() {
 		return "join";
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/startPage")
 	public String startPage() {
 		return "start_page";
 	}
-	
+
 	@GetMapping("/topbar")
 	public String topbar() {
 		return "top_bar";
 	}
-	
+
 	@GetMapping("/calendar")
 	public String calendar() {
 		return "calendar";
 	}
-	
+
 	@GetMapping("/mypage")
 	public String mypage() {
 		return "mypage";
 	}
-	
+
 	@GetMapping("/card")
 	public String card() {
 		return "card";
 	}
-	
+
 	@GetMapping("/account")
 	public String account() {
 		return "account";
 	}
-	
+
+	@GetMapping("/graph")
+	public String graph(Model model) {
+		GraphEntity turtle = repo.findPostureByMbIdAndPosType("adkim", "거북목");
+
+		model.addAttribute("turtle", turtle);
+		System.out.println("가져온 값은" + turtle.getPosCount() + turtle.getMbId());
+		return "graph";
+	}
+
+	@Autowired
+	GraphRepo repo;
+
+//	@GetMapping("/graph.do")
+//	public String graphDo(Model model) {
+//		GraphEntity turtle = repo.findPostureByMbIdAndPosType("adkim", "거북목");
+//
+//		model.addAttribute("turtle", turtle);
+//		System.out.println("가져온 값은" + turtle.getPosCount() + turtle.getMbId());
+//
+//		return "redirect:/";
+//	}
+
 }
